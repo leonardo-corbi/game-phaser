30c1f5eea55857e5776795e4e106fac5c8874e8a
leonardoccorbi <leocesarcorbi@gmail.com>
2025-02-19 10:03:39 -0300

    completed Week 1 game project
    Finalized the development of the Week 1 game project for the university. All core mechanics, features, and necessary adjustments have been implemented.

---
+++ sem-1/peixe.js
@@ -0,0 +1,35 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+var peixinho;
+
+function preload() {
+  this.load.image("mar", "assets/bg_azul-escuro.png");
+  this.load.image("logo", "assets/logo-inteli_branco.png");
+  this.load.image("peixe", "assets/peixes/peixinho_laranja.png");
+}
+
+function create() {
+  this.add.image(400, 300, "mar");
+  this.add.image(400, 525, "logo").setScale(0.5);
+
+  peixinho = this.add.image(400, 300, "peixe").setFlip(true, false);
+
+  this.add.text(30, 30, "Desenvolvido por Leonardo Corbi!");
+}
+
+function update() {
+  peixinho.x = this.input.x;
+  peixinho.y = this.input.y;
+}
